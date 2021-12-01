462f64e07be85708ac63755145ef179f9ce132d5
dsh410 <dsh410@gmail.com>
2021-12-01 15:20:55 -0600

    changed a tag a ref att to /.

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,10 @@ const NavBar = ({ title }) => {
       <h3 className=" title">
         {" "}
         {title}
-        <a href="#" className="log glow link">
+        <a href="/" className="log glow link">
           Login
         </a>
-        <a href="#" className="sign glow link">
+        <a href="/" className="sign glow link">
           Signup
         </a>
       </h3>
